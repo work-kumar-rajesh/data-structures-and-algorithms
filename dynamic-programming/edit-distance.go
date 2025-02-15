94fe4f0ab875896558255c05df77c40d0d508910
Rajesh Kumar <rajeshkumar@Rajeshs-MacBook-Air.local>
2025-02-15 10:19:37 +0530

    dp question : edit-distance

---
+++ dynamic-programming/edit-distance.go
@@ -0,0 +1,40 @@
+package main
+
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+// You have the following three operations permitted on a word:
+// Insert a character
+// Delete a character
+// Replace a character
+
+// Example 1:
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation:
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+
+func minDistance(word1 string, word2 string) int {
+	n := len(word1)
+	m := len(word2)
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 0; i <= n; i++ {
+		dp[i][0] = i
+	}
+	for i := 0; i <= m; i++ {
+		dp[0][i] = i
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = 1 + min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
+			}
+		}
+	}
+	return dp[n][m]
+}
