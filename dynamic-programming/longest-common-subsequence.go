4d8eb4ecce3c0ea6feb885ec1874b8814b42d4d4
Rajesh Kumar <rajeshkumar@Rajeshs-MacBook-Air.local>
2025-02-15 09:56:32 +0530

    dp question : longest common subsequence

---
+++ dynamic-programming/longest-common-subsequence.go
@@ -0,0 +1,27 @@
+package main
+
+// Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without
+// changing the relative order of the remaining characters.
+
+// For example, "ace" is a subsequence of "abcde".
+// A common subsequence of two strings is a subsequence that is common to both strings.
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	n := len(text1)
+	m := len(text2)
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = 1 + dp[i-1][j-1]
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[n][m]
+}
