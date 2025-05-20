c8c0a108793448b94fb74e2bf22695669c4d8970
work-kumar-rajesh <164179776+work-kumar-rajesh@users.noreply.github.com>
2025-05-20 07:31:03 +0530

    Update kth-smallest-in-bst.go

--- binary-search-tree/kth-smallest-in-bst.go
+++ binary-search-tree/kth-smallest-in-bst.go
@@ -1,21 +1,22 @@
 package main
 
 func kthSmallest(root *TreeNode, k int) int {
-	inOrder := make([]int, 0)
-	inOrder = inOrderTraversal(root, inOrder)
-	return inOrder[k-1]
+    result := -1
+   bfs(root, &k, &result)
+    return result
 }
 
-func inOrderTraversal(root *TreeNode, inOrder []int) []int {
-	if root == nil {
-		return nil
-	}
-	if root.Left != nil {
-		inOrder = inOrderTraversal(root.Left, inOrder)
-	}
-	inOrder = append(inOrder, root.Val)
-	if root.Right != nil {
-		inOrder = inOrderTraversal(root.Right, inOrder)
-	}
-	return inOrder
+func bfs(root *TreeNode, counter, result *int) {
+    if root == nil {
+        return
+    }
+
+    bfs(root.Left, counter, result)
+
+    *counter--
+    if *counter == 0 {
+        *result = root.Val
+        return 
+    }
+    bfs(root.Right, counter, result)
 }
